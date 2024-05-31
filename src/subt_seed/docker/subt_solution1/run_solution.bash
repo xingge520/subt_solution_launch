1165998e58f8c4099934e0d1cde499ca912a4b6e
SimonRain <63945969+xingge520@users.noreply.github.com>
2024-05-31 09:10:12 +0800

    Update run_solution.bash

--- src/subt_seed/docker/subt_solution1/run_solution.bash
+++ src/subt_seed/docker/subt_solution1/run_solution.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-source ${PATH_TO_SOLUTION_WS_SETUP_BASH}
+# source ${PATH_TO_SOLUTION_WS_SETUP_BASH}
+. /opt/ros/melodic/setup.bash
+. ~/subt_solution/install/setup.sh
 
 roslaunch subt_solution_launch robot.launch name:=X1
